0286ce34f1bf5c2a61ce8a0934c59a84b77a746f
Ghidorah <jljohnson1454@gmail.com>
2021-11-04 17:39:09 -0500

    added express session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,10 +60,19 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    // .then(dbUserData => res.json(dbUserData))
+    // .catch(err => {
+    //   console.log(err);
+    //   res.status(500).json(err);
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      
+    })
     });
 });
 
@@ -85,7 +94,12 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
-
+    req.session.save(() => {
+      //declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+    })
     res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
 });
@@ -131,5 +145,16 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+})
 
 module.exports = router;
